7f7036f7062e90913ab77b99c426baf0ba9a090c
Ravalika-500apps <ravalika.marampelli@500apps.com>
2023-04-11 14:29:55 +0530

    Added HTML Parser

---
+++ components/files/HtmlParser.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <span v-html="html"> </span>
+       <div v-html="root"></div>
+       <div v-html="test"></div>
+       
+    </div>
+</template>
+<script setup="ts">
+
+import { parse } from 'node-html-parser';
+ 
+const html = parse('<!doctype html><html><body onload="alert(\'Hii Everyone\')">Mountain</body></html>');
+const root = parse('<html><img width="570" src="https://www.w3schools.com/images/picture.jpg" alt="Mountain"></iframe></html>');
+
+
+</script>
